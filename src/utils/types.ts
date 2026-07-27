1ec59fff584065d4ac14e2095f35738defc095dd
shunLNY <shunlae@o-technique-myanmar.com>
2026-07-27 15:50:58 +0630

    article count in category edit

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,73 +1,74 @@
 /** @format */
 
-import { ArticleStatus } from './constants';
+import { ArticleStatus } from "./constants";
 
 export type LayoutProps = {
-  title: string;
-  className: string;
+	title: string;
+	className: string;
 };
 
 export type ArticlesInfoType = {
-  id: string,
-  createdAt: string,
-  updatedAt: string,
-  title: string,
-  status: string,
-  publish_start_at?: string | null,
-  publish_end_at?: string | null,
-  published_start_at?: string | null,
-  published_end_at?: string | null,
-  content: string,
-  excerpt: string | null,
-  description: string | null,
-  thumbnail_path: string,
-  creator_id: string,
-  editor_id: string,
-  creator: Account,
-  editor: Account,
-  blog_categories: { value: string; label: string }[]
-  categoryIds: string[]
-  categories: Category[]
+	id: string;
+	createdAt: string;
+	updatedAt: string;
+	title: string;
+	status: string;
+	publish_start_at?: string | null;
+	publish_end_at?: string | null;
+	published_start_at?: string | null;
+	published_end_at?: string | null;
+	content: string;
+	excerpt: string | null;
+	description: string | null;
+	thumbnail_path: string;
+	creator_id: string;
+	editor_id: string;
+	creator: Account;
+	editor: Account;
+	blog_categories: { value: string; label: string }[];
+	categoryIds: string[];
+	categories: Category[];
 };
 
 export type Category = {
-  id: string,
-  sort_order: number;
-  status: string;
-  category_name: string;
-  category_slug: string;
-  creatorId: string;
-  editorId: string;
-  blog_categories: any[];
-  article_count: number;
-  createdAt: string;
-  updatedAt: string;
-  creator: Account,
-  editor: Account,
-  category_id: string,
-  child_categories: any[];
-}
+	id: string;
+	sort_order: number;
+	status: string;
+	category_name: string;
+	category_slug: string;
+	creatorId: string;
+	editorId: string;
+	blog_categories: any[];
+	article_count: number;
+	number_of_articles_used: number;
+	createdAt: string;
+	updatedAt: string;
+	creator: Account;
+	editor: Account;
+	category_id: string;
+	child_categories: any[];
+};
 
 export type Account = {
-  id: string,
-  account_id: string,
-  role: string;
-  account_name: string;
-  email: string;
-  password: string;
-}
+	id: string;
+	account_id: string;
+	role: string;
+	account_name: string;
+	email: string;
+	password: string;
+};
 
 export type Tenant = {
-  id: string;
-  name: string;
-  domain?: string;
-}
+	id: string;
+	name: string;
+	domain?: string;
+};
 
 export type PageMeta = {
-  current_page: number;
-  last_page: number;
-  per_page: number;
-  total: number;
-}
+	current_page: number;
+	last_page: number;
+	per_page: number;
+	total: number;
+};
 
-export type AuthScreen = 'emailScreen' | 'passwordScreen' | 'successScreen';
+export type AuthScreen = "emailScreen" | "passwordScreen" | "successScreen";
